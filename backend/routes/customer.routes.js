8a37ac64cff346ae17a628321657d4a2bbb14573
Saleamlak21 <fentaneshmesfin@gmail.com>
2024-01-27 13:01:45 -0500

    export the customer route

--- backend/routes/customer.routes.js
+++ backend/routes/customer.routes.js
@@ -1,8 +1,14 @@
 //import express from 'express';
 const express = require('express');
-//call the router method from express to create the router
+//call the router method from express to create the router  
 const router = express.Router();
 //import the customer controller
 const customerController = require('../controllers/customer.controller');
 //create a route to handle the add customer request on post
 router.post("/api/customer", customerController.createCustomer);
+
+
+
+
+//export the router
+module.exports = router;
